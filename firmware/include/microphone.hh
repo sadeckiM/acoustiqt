236594cd884d3c1e994ccce3cccbb544925da5e1
Cardaver <michal.sadecki@proton.me>
2026-04-09 12:04:29 +0200

    feat: add API for signal processing

--- firmware/include/microphone.hh
+++ firmware/include/microphone.hh
@@ -10,11 +10,16 @@
 class Microphone {
   i2s_config_t i2s_config;
   i2s_pin_config_t i2s_mic_pins;
+  int32_t buffer[BUFFER_SIZE];
+  int32_t last_sample;
   inline void togglePin(int pin) { digitalWrite(pin, !digitalRead(pin)); }
+  int32_t read_raw_data_to_buffer();
 public:
-  Microphone(int pin_sck, int pin_ws, int pin_sd, i2s_channel_fmt_t channel,
-             ulong baud_rate);
-  int32_t get_amplitude(uint8_t shift = 14);
+  Microphone(int32_t pin_sck, int32_t pin_ws, int32_t pin_sd, i2s_channel_fmt_t
+             channel);
+  int32_t get_rms(uint8_t shift = 14);
+  inline const int32_t* get_buffer() const { return buffer; }
+  inline const size_t get_buffer_sample_count() const { return last_sample; }
 };
 
 #endif
--- firmware/src/microphone.cpp
+++ firmware/src/microphone.cpp
@@ -1,7 +1,6 @@
 #include "microphone.hh"
 
-Microphone::Microphone(int pin_sck, int pin_ws, int pin_sd, i2s_channel_fmt_t
-                       channel, ulong baud_rate) {
+Microphone::Microphone(int32_t pin_sck, int32_t pin_ws, int32_t pin_sd, i2s_channel_fmt_t channel) {
   i2s_config = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
     .sample_rate = SAMPLE_RATE,
@@ -15,7 +14,6 @@ Microphone::Microphone(int pin_sck, int pin_ws, int pin_sd, i2s_channel_fmt_t
     .tx_desc_auto_clear = false,
     .fixed_mclk = 0
   };
-
   i2s_mic_pins = {
     .bck_io_num = pin_sck,
     .ws_io_num = pin_ws,
@@ -24,20 +22,26 @@ Microphone::Microphone(int pin_sck, int pin_ws, int pin_sd, i2s_channel_fmt_t
   };
   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
   i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
-  Serial.begin(baud_rate);
+  Serial.begin(SERIAL_BAUD);
 }
 
-int32_t Microphone::get_amplitude(uint8_t shift) {
-  size_t bytes_read = 0;
-  int32_t raw_samples[BUFFER_SIZE];
+int32_t Microphone::get_rms(uint8_t shift) {
   int64_t sum = 0;
-  i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * BUFFER_SIZE, &bytes_read,
-           portMAX_DELAY);
-  int samples_read = bytes_read / sizeof(int32_t);
-  if(samples_read == 0) return 0;
-
-  for (int i = 0; i < samples_read; ++i) {
-    sum += std::abs(raw_samples[i] >> shift);
+  for (int32_t i = 0; i < last_sample; ++i) {
+    int32_t sample = buffer[i] >> shift;
+    sum += sample * sample;
   }
-  return static_cast<int32_t>(sum/samples_read);
+  return static_cast<int32_t>(std::sqrt(sum/last_sample));
+}
+
+int32_t Microphone::read_raw_data_to_buffer() {
+  size_t bytes_read = 0;
+  esp_err_t err = i2s_read(I2S_NUM_0, buffer, sizeof(int32_t) * BUFFER_SIZE, &bytes_read, portMAX_DELAY); 
+  if (err != ESP_OK) {
+    Serial.printf("I2S: read_raw: %s\n", esp_err_to_name(err));
+    last_sample = 0;
+    return -1;
+  }
+  last_sample = static_cast<int32_t>(bytes_read / sizeof(int32_t));
+  return last_sample;
 }
